9f7a0f3a76b2d64b40ad41d2d08775884ecb4273
adelyneyana <126838952+adelyneyana@users.noreply.github.com>
2023-11-22 21:40:19 +0800

    Add files via upload

---
+++ src/style/styles.js
@@ -0,0 +1,65 @@
+const globalstyles = {
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  headerContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 20, 
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    zIndex: 1,
+  },
+  logo: {
+    width: 400, 
+    height: 160, 
+    justifyContent: 'center',
+  },
+  inputContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 16,
+  },
+  input: {
+    marginBottom: 10,
+  },
+  addButton: {
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  listHeaderText: {
+    margin: 10,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  scrollView: {
+    flex: 1,
+  },
+  listItem: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderRadius: 50,
+    padding: 5,
+    marginVertical: 2,
+  },
+  listItemText: {
+    fontSize: 20,
+    marginBottom: 8,
+    marginTop: 6,
+  },
+  dueDateText: {
+    marginBottom: 10,
+  },
+  removeButton: {
+    marginTop: 25,
+    marginBottom: 10,
+    backgroundColor: 'red',
+  },
+  };
+  
+  export default globalstyles;
